644453c160973669504acc1d01ca84707a383a97
learnitskills <64744608+learnitskills@users.noreply.github.com>
2022-03-20 16:02:12 +0530

    Update template.tf

--- module/template.tf
+++ module/template.tf
@@ -5,7 +5,7 @@ module "instance" {
   subnetnames           = ["ECN-SUBNET-2", "ECN-SUBNET-4", "ECN-SUBNET-5"]
   hostname              = ["Server1", "Server2", "Server3"]
   termination           = false
-  kms_key_arn           = "arn:aws:kms:us-east-1:229871141864:key/fbfacd0d-a394-4c38-9f98-acac74ec08e3"
+  kms_key_arn           = "your kms key arn"
   secondary_volume_size = [1, 2, 3]
 
 }
